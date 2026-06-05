fb7c61f4be814cb4271aa794127f832d58160d76
Gregory Herschlag <gjh@math.duke.edu>
2026-06-05 12:52:17 -0400

    update

---
+++ src/measure/constraints/cap_region_districts/cap_region_districts_type.jl
@@ -0,0 +1,51 @@
+struct CapRegionDistricts <: AbstractConstraint
+    region_to_nodes::Dict{String, Vector{Int}}
+    region_to_dist_cap::Dict{String, Int}
+    region::String
+    desc::String
+    ideal_pop::T where T <: Real
+end
+
+const CapRegionDistConstraint = CapRegionDistricts
+
+""""""
+function CapRegionDistricts(
+    graph::Graph,
+    region::String;
+    excess_split::Int=0,
+    num_dists::Int=0,
+    ideal_pop::Real=0
+)::CapRegionDistricts
+    if ideal_pop == 0 && num_dists == 0
+        throw(
+            ArgumentError("Need to specify either ideal_pop or num_dists",)
+        )
+    elseif ideal_pop == 0
+        ideal_pop = graph.graphs_by_level[end].total_pop / num_dists
+    end
+    base_graph = graph.graphs_by_level[end]
+
+    regions_pop = Dict{String, T where T<:Real}()
+    region_nodes = Dict{String, Vector{Int}}()
+    region_to_dist_cap = Dict{String, Int}()
+    pop_col = base_graph.pop_col
+
+    for node_id = 1:base_graph.num_nodes
+        region_name = base_graph.node_attributes[node_id][region]
+
+        node_pop = base_graph.node_attributes[node_id][pop_col]
+        regions_pop[region_name] = get(regions_pop, region_name, 0) + node_pop
+
+        region_nodes[region_name] = get(region_nodes, region_name, Int[])
+        push!(region_nodes[region_name], node_id)
+    end
+
+    for (region_name, pop) in regions_pop
+        min_touching_districts = Int(ceil(pop / ideal_pop))
+        region_to_dist_cap[region_name] = min_touching_districts + excess_split
+    end
+
+    return CapRegionDistricts(region_nodes, region_to_dist_cap, region,
+                              region * "_excess_split" * string(excess_split),
+                              ideal_pop)
+end
