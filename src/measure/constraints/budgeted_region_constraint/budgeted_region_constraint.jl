dd9ab2e5025ff0c4535d8d52432a95475cdae902
Franklin Zhou <xz455@duke.edu>
2026-06-14 11:44:17 -0500

    Add `BudgetedRegionConstraint` based on `PackRegionConstraint` and `CapRegionDistricts`

---
+++ src/measure/constraints/budgeted_region_constraint/budgeted_region_constraint.jl
@@ -0,0 +1,98 @@
+function satisfies_constraint(
+    partition::LinkCutPartition,
+    brc::BudgetedRegionConstraint,
+    districts::Union{Tuple{Vararg{T}}, Vector{T}}
+        = collect(1:partition.num_dists);
+    update::Union{Update, Nothing}=nothing
+)::Bool where T<:Int
+    region = (brc.region,)
+
+    if !haskey(partition.energy_data, (RegionalSplitData, region))
+        data = RegionalSplitData(partition, brc.region)
+        partition.energy_data[(RegionalSplitData, region)] = data
+    end
+
+    data = partition.energy_data[(RegionalSplitData, region)]
+
+    if partition.identifier != data.identifier
+        data = RegionalSplitData(partition, brc.region)
+        partition.energy_data[(RegionalSplitData, region)] = data
+    end
+
+    if update !== nothing && update.identifier != data.update_identifier
+        update_regional_split_data!(data, partition, update)
+    end
+
+    active_districts = districts
+    active_regions_to_dists = data.regions_to_dists
+    active_dists_to_regions = data.dists_to_regions
+
+    if update !== nothing
+        active_districts = collect(update.changed_districts)
+        active_regions_to_dists = data.regions_to_dists_update
+        active_dists_to_regions = data.dists_to_regions_update
+    end
+
+    return satisfies_constraint(
+        brc,
+        active_districts,
+        active_regions_to_dists,
+        active_dists_to_regions,
+    )
+end
+
+function satisfies_constraint(
+    brc::BudgetedRegionConstraint,
+    districts::Union{Tuple{Vararg{T}}, Vector{T}},
+    regions_to_dists::Dict{String, Set{Int64}},
+    dists_to_regions::Dict{Int64, Set{String}},
+)::Bool where T<:Int
+    checked_regions = Set{String}()
+
+    pack_excess = 0
+    cap_excess = 0
+
+    for district in districts
+        district_regions = dists_to_regions[district]
+
+        for region in district_regions
+            if region in checked_regions
+                continue
+            end
+            push!(checked_regions, region)
+
+            if haskey(brc.region_to_packed_dists, region)
+                req_pack = brc.region_to_packed_dists[region]
+
+                exclusive_count = 0
+                for di in regions_to_dists[region]
+                    di_regions = dists_to_regions[di]
+                    if length(di_regions) == 1 && region in di_regions
+                        exclusive_count += 1
+                    end
+                end
+
+                pack_excess += max(0, req_pack - exclusive_count)
+
+                if pack_excess > brc.pack_budget
+                    return false
+                end
+            end
+
+            if haskey(brc.region_to_dist_cap, region)
+                touched_count = length(regions_to_dists[region])
+                cap_excess += max(0, touched_count - brc.region_to_dist_cap[region])
+
+                if cap_excess > brc.cap_budget
+                    return false
+                end
+            end
+
+            if pack_excess + cap_excess > brc.total_budget
+                return false
+            end
+        end
+    end
+
+    return true
+end
